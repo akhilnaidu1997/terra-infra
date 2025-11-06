807823b6576f88763d36c4bbafb679018680ba6a
Akhil Naidu <akhilnaid1997@gmail.com>
2025-11-06 10:55:58 +0530

    message

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -6,7 +6,6 @@ locals {
   }
   common_name = "${var.project}-${var.environment}"
   ami = data.aws_ami.ami.id
-  mongodb = data.aws_ssm_parameter.sg_id.value
   private = split(",",data.aws_ssm_parameter.private_subnets.value)
   catalogue = data.aws_ssm_parameter.catalogue.value
 }
